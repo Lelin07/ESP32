bbebdb6a574376395ba4815778b7ee8bef1a8ee2
Lelin07 <lelinpadhan27@gmail.com>
2025-03-13 19:38:05 +0530

    Define LED and buzzer pins, and implement basic setup and loop functionality

--- ISS_Tracker/src/main.cpp
+++ ISS_Tracker/src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_BUILTIN 2 // Define the built-in LED pin for ESP32
+#define BUZZER_PIN 25 // Define the buzzer pin for ESP32
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize digital pin BUZZER_PIN as an output.
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // turn the buzzer on
+  digitalWrite(BUZZER_PIN, HIGH);
+  // wait for a second
+  delay(200);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  // turn the buzzer off
+  digitalWrite(BUZZER_PIN, LOW);
+  // wait for a second
+  delay(3000);
 }
